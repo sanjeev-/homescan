d0858fdeb0d83d5a267a1c1072676df9d2af5357
sanjeev- <sanjeev87@gmail.com>
2018-02-08 11:22:26 -0500

    updating with new formulas

--- utils/remax_scrape.py
+++ utils/remax_scrape.py
@@ -1,106 +1,126 @@
 from scraping_utils import *
-import pandas as pd 
+import pandas as pd
 import argparse    
 from bs4 import BeautifulSoup
 import time
 import os
 import sys
 import subprocess
-import datetime
+from datetime import datetime
 import pickle
 import pandas as pd
 from django.core.wsgi import get_wsgi_application
 import json
 from dateutil import parser as dateparser
-#import urllib3
+from load_properties import fetch_from_google_storage
+from load_properties import find_latest_csvname
+from dateutil import parser
 
-#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-# defining argparse
-parser = argparse.ArgumentParser(description='Process some arguments')
-
-parser.add_argument('--city',metavar='C',type=str,help='city that you want to scrape e.g. "charlotte" ')
-parser.add_argument('--state',metavar='S',type=str,help='two letter abbrv of the state of the city you want to scrape e.g. "NC"')
-
-arg_check = parser.parse_args()
-args = vars(parser.parse_args())
-
-def find_latest_csvname(specific_date='<YYYYMMDD>'):
-    """Finds the newest csv name in the drive.  Can pull a specific date
-       by filling in the specific date field.
+def df_filename():
+    """This returns csv filename for today
 
     Keyword arguments:
-        specific date: [string in YYYYMMDD date format ] This is optional.  Standard it to leave blank and it will 
-                        pull the latest.
+        None
 
     Returns:
-        returns a csv filename to pull from google storage bucket.
+        df_filename: [string] csv filename data_[todays_date].csv
     """
-    command = ['gsutil','ls','gs://rooftop-data/properties_data/']
-    out = subprocess.check_output(command)
-    csv_list = str(out).split('\\n')
-    print('csv_list: {}'.format(csv_list))
-    parsed_csv_list = [x[x.find('data_')+5:-4] for x in csv_list]
-    print('parsed_csv_list:{}'.format(parsed_csv_list))
-    date_csv_list = [x for x in parsed_csv_list if len(x)==8]
-    print('date_csv_list: {}'.format(date_csv_list))
-    dates = [dateparser.parse(x) for x in date_csv_list]
-    print('dates: {}'.format(dates))
-    now = datetime.datetime.now()
-    newest = max(dt for dt in dates if dt < now)
-    csv_filename = 'data_{}.csv'.format(newest.strftime('%Y%m%d'))
-    return csv_filename
+    now = datetime.now()
+    datestr = now.strftime('%Y%m%d')
+    df_filename = 'data_{}.csv'.format(datestr)
+    return df_filename
 
-def fetch_from_google_storage(google_storage_bucket, path_to_file, filename, destination_folder):
-    """Pulls file from google cloud storage bucket
-    
+def implement_arg_parse():
+    """This implements the command line arguments
     Keyword arguments:
-        google_storage_bucket: [string] name of google storage bucket e.g. django-uploads
-        path_to_file: [string] path to the folder of the file you want to pull from
-        google storage
-        filename: [string] name of the file you want to pull e.g. data_<YYYYMMDD>.csv
-        destination_folder: [string] path to the folder you want to save the pulled file in
+        None
     
     Returns:
-    returns success status if successful
+        Implements --city: [cl argument] city you want to scrape
+                   --state: [cl argument] state you want to scrape
+
+                   as command line arguments
     """
-    pull_path = 'gs://{}/{}/{}'.format(google_storage_bucket, path_to_file, filename)
-    print('pull path is {}'.format(pull_path))
-    destination_path = os.getcwd() + '/{}/{}'.format(destination_folder, filename)
-    print('destination path is {}'.format(destination_path))
-    gsutil_command = ['gsutil', 'cp', pull_path,destination_path]
-    try:
-        subprocess.call(gsutil_command)
-        print('pulled file {} successfully from the {} google storage bucket'.format(filename,google_storage_bucket))
-    except Exception as e:
-        print('pull failed. error message: {}'.format(e))
+    parser = argparse.ArgumentParser(description='Process some arguments')
+    parser.add_argument('--city',metavar='C',type=str,help='city that you want to scrape e.g. "charlotte" ')
+    parser.add_argument('--state',metavar='S',type=str,help='two letter abbrv of the state of the city you want to scrape e.g. "NC"')
+    arg_check = parser.parse_args()
+    args = vars(parser.parse_args())
+    return arg_check, args
 
+def find_last_scrape_date(df):
+    """Find the date of the most recently scraped property in the csv
 
-def scrapeRemax(city,state):
-    csv_filename = find_latest_csvname()
-    fetch_from_google_storage('rooftop-data','properties_data',csv_filename,
-                              'csv_data')
+    Keyword arguments:
+        df: [pandas dataframe] dataframe of scraped properties from last scraped csv
+
+    Returns:
+        datetime of the most recently scraped property in the dataframe
+    """
+    df['features_start_date_on_site']=df['features_start_date_on_site'].apply(parser.parse)
+    latest_date = max(df['features_start_date_on_site'])
+    latest_date_datetime = latest_date.to_datetime()
+    return latest_date_datetime
+
+def check_date_vs_last_scrape_date(property_date,last_scrape_date):
+    """Takes the date of a scraped property and compares it to the last scraped date
+       in old dateframe
+
+    Keyword arguments:
+        property_date: [datetime] the date of a remax property to be scraped
+        last_scrape_date: [datetime] the date of the most recently scraped property
+                                     in the old dataframe
+
+    Returns:
+        is_ahead_of_last_scrape_date: [Boolean] True if more recent, else False
+    """
+    is_ahead_of_last_scrape_date = property_date >= last_scrape_date
+    return is_ahead_of_last_scrape_date
+
+def scrape_remax(city,state):
+    """Loops through pages of Remax site, scraping data and stopping when hits StopDate
+
+    Keyword arguments:
+        city: [string] city of area you want to scrape
+        state: [string] state of the area you want to scrape
+        stop_date: [string] string (YYYY-MM-DD) of the date at which you wish to 
+                            stop scraping
+
+    Returns:
+        csv of the dataframe
+    """
     df_old = pd.read_csv('csv_data/'+csv_filename)
+    last_scrape_date = find_last_scrape_date(df_old)
     d={}
-    for pg in range(1,15):
+    pg = 0
+    keep_on_scraping = True
+    while keep_on_scraping:
+        pg += 1
         print('scraping page %s' % (str(pg)))
         page = createRemaxCityURL(city,state,pg)
         urls = findReMaxURLS(BeautifulSoup(get(page,verify=False).text,'html.parser'))
         for url in urls:
             try:
-                print('napping for three seconds')
+                print('napping for a bit')
                 time.sleep(1.5)
+                print(keep_on_scraping)
                 home = pullHomeData(url)
                 flat = flatten_dict(home)
                 slug = flat['address_slug']
                 d[slug] = flat
+                property_date = parser.parse(home['features_start_date_on_site']).to_datetime()
+                keep_on_scraping = check_date_vs_last_scrape_date(property_date,last_scrape_date)
+                print('current propery date is: {}  last df property date is: {}'.format(property_date,last_scrape_date))
             except:
-                pass
-    df_new = pd.DataFrame.from_dict(d,orient='index')
-    df_combined = pd.concat([df_old, df_new])
-    df_combined.to_csv('data_20180129.csv',encoding='utf-8')
+                    pass
+        df_new = pd.DataFrame.from_dict(d,orient='index')
+        df_combined = pd.concat([df_old, df_new])
+        new_df_filename = df_filename()
+        df_combined.to_csv(new_df_filename,encoding='utf-8')
+    print('scraping update completed!')
 
 if __name__ == '__main__':
+    arg_check, args = implement_arg_parse()
     if arg_check.city == 'check_string_for_empty':
         print('you didnt supply a city.  please try again.')
     if arg_check.state == 'check_string_for_empty':
@@ -108,5 +128,7 @@ if __name__ == '__main__':
     city=args['city']
     state=args['state']
     print('scraping home data for %s, %s :  time elapsed is...' % (city, state))
-
-    scrapeRemax(city, state)
+    csv_filename = find_latest_csvname()
+    fetch_from_google_storage('rooftop-data','properties_data',csv_filename,
+                              'csv_data')
+    scrape_remax(city, state)
--- utils/sold_homes_scrape.py
+++ utils/sold_homes_scrape.py
@@ -1,49 +1,114 @@
 from scraping_utils import *
 import pandas as pd 
 from bs4 import BeautifulSoup
+from load_properties import fetch_from_google_storage
+from load_properties import find_latest_csvname
+from load_soldproperties import find_latest_soldpx_csvname
 import time
 import random
 import urllib3
+from remax_scrape import df_filename, implement_arg_parse, find_last_scrape_date, check_date_vs_last_scrape_date
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def check_slug_in_dataframe(slug,df):
+    """This checks if a slug is in a dataframe
 
-# defining argparse
+    Keyword arguments:
+        slug: [string] this is the slug you want to check if is already in the dataframe
+        df: [pandas dataframe] this is the dataframe you want to check against
 
-
-
-def scrapeSoldHomes(new_csv_interval=10):
+    Returns:
+        exists_in_df: [boolean] True is slug is already in dataframe, else False
     """
-    new csv interval defines after how many homes we save a new csv datasheet
+    exists_in_df = df['Unnamed: 0'].str.contains(slug).sum() >= 1
+    return exists_in_df
+
+def send_to_google_storage(google_storage_bucket, path_to_file, filename, destination_folder):
+    """Sends a file to google cloud storage bucket from local drive
+    
+    Keyword arguments:
+        google_storage_bucket: [string] name of google storage bucket e.g. django-uploads
+        path_to_file: [string] path to the folder of the file you want to pull from
+        google storage
+        filename: [string] name of the file you want to pull e.g. data_<YYYYMMDD>.csv
+        destination_folder: [string] path to the folder you want to save the pulled file in
+    
+    Returns:
+    returns success status if successful
+    """
+    gs_path = 'gs://{}/{}/{}'.format(google_storage_bucket, path_to_file, filename)
+    print('google storage path is {}'.format(gs_path))
+    local_path = os.getcwd() + '/{}/{}'.format(destination_folder, filename)
+    print('local file path is {}'.format(local_path))
+    gsutil_command = ['gsutil', 'cp', local_path,gs_path]
+    try:
+        subprocess.call(gsutil_command)
+        print('pulled file {} successfully from the {} google storage bucket'.format(filename,google_storage_bucket))
+    except Exception as e:
+        print('pull failed. error message: {}'.format(e))
+
+def solddf_filename():
+    """This returns csv filename for today
+
+    Keyword arguments:
+        None
+
+    Returns:
+        df_filename: [string] csv filename data_[todays_date].csv
+    """
+    now = datetime.now()
+    datestr = now.strftime('%Y%m%d')
+    df_filename = 'soldhomedata_{}.csv'.format(datestr)
+    return df_filename
+
+def scrape_sold_homes():
+    """Scrapes remax sold homes
+
+    Featured arguments:
+        None
+
+    Returns:
+        A csv of all the new sold homes, appended to the old soldataframme
 
     """
+    df_old = pd.read_csv('csv_data/'+csv_filename)
+    keep_on_scraping = True
+    pg = 0
     d = {}
-    for pg in range(0,2000):
+    while keep_on_scraping:
+        pg += 1
         home_number = len(d)
-        fails = 0
-        tried = 0
         pageurl = createSoldHomeURLNoFilter(pg)
         pagesoup = BeautifulSoup(get(pageurl,verify=False).text,'html.parser')
         urls = findReMaxURLS(pagesoup)
         for url in urls:
-            print(url)
             rand_nap = random.uniform(0,2)
             print('sleeping for a random %f seconds'% (rand_nap))
             time.sleep(rand_nap)
-            tried +=1
             try:
                 soldhome = pullSoldHomeData(url)
+                slug = soldhome['Unnamed: 0']
                 flat = flatten_dict(soldhome)
                 slug = flat['address_slug']
                 d[slug] = flat
-            except:
-                fails +=1
-                print('adding home failed!  failure num_fails is {}, fail_rate is: {}'.format(str(fails),float(fails/tried)))
-                print('failure_rate:{}'.format(float(fails)/float(tried)))
-        if pg % 20  == 0:
-            csv_output_name = 'sold_home_page_{}.csv'.format(str(pg))
-            df = pd.DataFrame.from_dict(d,orient='index')
-            df.to_csv(csv_output_name,encoding=('utf-8'))
-
+                keep_on_scraping = check_slug_in_dataframe(slug,df_old)
+            except Exception as e:
+                print(e)
+        df_new = pd.DataFrame.from_dict(d,orient='index')
+        df_combined = pd.concat([df_old, df_new])
+        new_df_filename = solddf_filename()
+        df_combined.to_csv(new_df_filename,encoding='utf-8')
 
 if __name__ == '__main__':
-    scrapeSoldHomes()
+    arg_check, args = implement_arg_parse()
+    if arg_check.city == 'check_string_for_empty':
+        print('you didnt supply a city.  please try again.')
+    if arg_check.state == 'check_string_for_empty':
+        print('you didnt supply a state.  please try again')
+    city=args['city']
+    state=args['state']
+    print('scraping home data for %s, %s :  time elapsed is...' % (city, state))
+    csv_filename = find_latest_soldpx_csvname()
+    fetch_from_google_storage('rooftop-data','sold_home_data',csv_filename,
+                              'csv_data')
+    scrape_sold_homes()
